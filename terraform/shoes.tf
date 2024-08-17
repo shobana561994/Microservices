15666d8a642dbb1796dab078158b21e51bdab8cf
shobana561994 <116883936+shobana561994@users.noreply.github.com>
2024-08-18 00:37:59 +0530

    Update shoes.tf

--- terraform/shoes.tf
+++ terraform/shoes.tf
@@ -23,7 +23,7 @@ resource "kubernetes_deployment" "shoe" {
       }
       spec {
         container {
-          image = "comdevops/spring:v2"
+          image = "shobana56it/spring:v2"
           name  = "shoe"
           port {
             container_port = 1002

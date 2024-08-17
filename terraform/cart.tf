89c2e2ec99c7ed103f636148ceb01fd125e823be
shobana561994 <116883936+shobana561994@users.noreply.github.com>
2024-08-18 00:36:20 +0530

    Update cart.tf

--- terraform/cart.tf
+++ terraform/cart.tf
@@ -23,7 +23,7 @@ resource "kubernetes_deployment" "cart" {
       }
       spec {
         container {
-          image = "comdevops/ui:v2"
+          image = "shobana56it/ui:v2"
           name  = "cart"
           port {
             container_port = 1004
